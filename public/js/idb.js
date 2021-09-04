61253f6dd4ed7de056428d6c764ef7bf87a5b6f1
Abhishek Jamwal <jamwal.ab@gmail.com>
2021-09-04 02:11:26 -0400

    index db save data and send function added

--- public/js/idb.js
+++ public/js/idb.js
@@ -28,7 +28,14 @@ request.onerror = function(event) {
 
 //function to save record if there is no internet connection
 function saveData(data) {
+  // open a new transaction with the database with read and write permissions 
+  const transaction =  db.transaction(['budget_data'], 'readwrite');
 
+  //access the object store
+  const transactionObjectStore = transaction.objectStore('budget_data');
+
+  //add record to the table
+  transactionObjectStore.add(data)
 };
 
 //function to upload data to the database
@@ -37,5 +44,42 @@ function uploadBudget(event) {
   const transaction = db.transaction(['budget_data'], 'readwrite');
 
   //access the object store
-  
+  const transactionObjectStore = transaction.objectStore('budget_data');
+
+  //get all records from the table
+  const getAll = transactionObjectStore.getAll();
+
+  getAll.onsuccess = function() {
+    //check if there is data in the getAll variable
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json"
+        }
+      })
+      .then(response => {    
+        return response.json();
+      })
+      .then(serverResponse => {
+        //Check for error, and stop if any
+        if (serverResponse.message) {
+          throw new Error(serverResponse);
+        }
+        //open another transaction
+        const transaction = db.transaction(['budget_data'], 'readwrite');
+        //access the objectstore again
+        const transactionObjectStore = transaction.objectStore('budget_data');
+        //clear the table
+        transactionObjectStore.clear();
+
+        console.log('All transaction data has been successfully recorded');
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    }
+  }
 }
--- public/js/index.js
+++ public/js/index.js
@@ -136,7 +136,7 @@ function sendTransaction(isAdding) {
   })
   .catch(err => {
     // fetch failed, so save in indexed db
-    saveRecord(transaction);
+    saveData(transaction);
 
     // clear form
     nameEl.value = "";

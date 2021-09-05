5683819a9bf935cb7c28ae30b662eb9e0d051abc
Abhishek Jamwal <jamwal.ab@gmail.com>
2021-09-05 00:19:58 -0400

    service worker code added with manifest

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,32 +5,28 @@ const CACHE_NAME = APP_PREFIX + VERSION;
 
 //Files to be cached
 const FILES_TO_CACHE = [
-  './index.html',
-  './css/styles.css',
-  './js/index.js',
-  './js/idb.js',
-  'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
-  'https://cdn.jsdelivr.net/npm/chart.js@2.8.0'
+  '/index.html',
+  '/css/styles.css',
+  '/js/index.js',
+  '/js/idb.js'
 ];
 
 // Respond with cached resources
 self.addEventListener('fetch', function (e) {
   console.log('fetch request : ' + e.request.url)
   e.respondWith(
-    caches.match(e.request)
-      .then(function (request) {
-        if (request) { // if cache is available, respond with cache
-          console.log('responding with cache : ' + e.request.url)
-          return request
-        } else {       // if there are no cache, try fetching request
-          console.log('file is not cached, fetching : ' + e.request.url)
-          return fetch(e.request)
-        }
+    caches.match(e.request).then(function (request) {
+      if (request) { // if cache is available, respond with cache
+        console.log('responding with cache : ' + e.request.url)
+        return request
+      } else {       // if there are no cache, try fetching request
+        console.log('file is not cached, fetching : ' + e.request.url)
+        return fetch(e.request)
+      }
 
-        // You can omit if/else for console.log & put one line below like this too.
-        // return request || fetch(e.request)
-      })
-
+      // You can omit if/else for console.log & put one line below like this too.
+      // return request || fetch(e.request)
+    })
   )
 })
 
@@ -45,25 +41,23 @@ self.addEventListener('install', function (e) {
 })
 
 // Delete outdated caches
-self.addEventListener('activate', function(e) {
+self.addEventListener('activate', function (e) {
   e.waitUntil(
-    caches.keys().then(function(keyList) {
+    caches.keys().then(function (keyList) {
       // `keyList` contains all cache names under your username.github.io
       // filter out ones that has this app prefix to create keeplist
-      let cacheKeeplist = keyList.filter(function(key) {
+      let cacheKeeplist = keyList.filter(function (key) {
         return key.indexOf(APP_PREFIX);
-      });
+      })
       // add current cache name to keeplist
       cacheKeeplist.push(CACHE_NAME);
 
-      return Promise.all(
-        keyList.map(function(key, i) {
-          if (cacheKeeplist.indexOf(key) === -1) {
-            console.log('deleting cache : ' + keyList[i]);
-            return caches.delete(keyList[i]);
-          }
-        })
-      );
+      return Promise.all(keyList.map(function (key, i) {
+        if (cacheKeeplist.indexOf(key) === -1) {
+          console.log('deleting cache : ' + keyList[i] );
+          return caches.delete(keyList[i]);
+        }
+      }));
     })
   );
 });
